85e4cd4dc421d6b8484bc1e63f46d12652df2cf4
Abhishek <abhishek.trancis@gmail.com>
2018-09-17 22:40:15 -0700

    api

---
+++ src/services/commanhttp.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Events } from 'ionic-angular';
+import { Observable } from 'rxjs/Rx';
+
+@Injectable()
+export class commanapi {
+        
+
+    private _download_process : any = [];
+    
+    /**
+     * Constructor
+     */
+    constructor() {
+        
+    }
+
+}
